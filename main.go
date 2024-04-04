68bed3b15086a00a7c19628a779508a15be2cf6c
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-04 19:45:31 +0900

    constants and variables

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	const name string = "yj" // constant
+	// var companyName string = "awesome studio" // variable
+	companyName := "awesome studio" // variable
+	companyName = "not awesome studio" // reassign variable
 }
