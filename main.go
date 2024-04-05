f7d2b0dabf783e3e84b21fa365a6c1d44520687f
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-05 13:04:22 +0900

    functions 1

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -9,4 +12,23 @@ func main() {
 	// var companyName string = "awesome studio" // variable
 	companyName := "awesome studio" // variable
 	companyName = "not awesome studio" // reassign variable
+
+	fmt.Println(multiply(2, 3))
+
+	length, upperCaseName := lenAndUpper("yj")
+	fmt.Println(length, upperCaseName)
+
+	shoutOut("yj", "123", "7890")
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+func shoutOut(names ...string) {
+	fmt.Println(names)
 }
