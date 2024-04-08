b040e638f978880703378bb9e33ad90f5ea5621a
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-08 13:26:44 +0900

    maps (simple)

--- main.go
+++ main.go
@@ -6,14 +6,12 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
-	// with length: array
-	numbers := [3]int{1, 2, 3}
-	fmt.Println(numbers)
-	// without length: slice
-	nums := []int{1, 2, 3}
-	nums = append(nums, 4)
-	fmt.Println(nums)
-	
+	// map
+	personMap := map[string]string{"name": "Q", "age": "20"}
+	fmt.Println(personMap)
+	for key, index := range personMap {
+		fmt.Println(key, index)
+	}
 }
 
 
