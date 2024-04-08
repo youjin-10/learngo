5c05bc51d7166b7f99e9f70d4961a05156272b0e
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-08 13:21:35 +0900

    arrays & slices

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 
 func main() {
 	fmt.Println("Hello, World!")
-	a := 2
-	b := &a
-	*b = 123
-	fmt.Println(a, *b)
+	// with length: array
+	numbers := [3]int{1, 2, 3}
+	fmt.Println(numbers)
+	// without length: slice
+	nums := []int{1, 2, 3}
+	nums = append(nums, 4)
+	fmt.Println(nums)
+	
 }
 
 
