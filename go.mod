75135b1c5b344187c1eec7cce296a5a50fa5dfd8
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-10 23:04:03 +0900

    structs - constructing structs

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/youjin-10/learngo
+
+go 1.22.1
--- main.go
+++ main.go
@@ -2,42 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/youjin-10/learngo/accounts"
 )
 
-type person struct {
-	name      string
-	age       int
-	isStudent bool
-	favFruit  []string
-}
-
 func main() {
-	fmt.Println("Hello, World!")
-	// map
-	personMap := map[string]string{"name": "Q", "age": "20"}
-	fmt.Println(personMap)
-	for key, index := range personMap {
-		fmt.Println(key, index)
-	}
-
-	/*
-	{
-		"name": "Q",
-		"age": 20,
-		"isStudent": true,
-		"favFruit": ["apple", "banana"],
-	}
-	*/
-	// struct
-	p := person{
-		name:      "Q",
-		age:       20,
-		isStudent: true,
-		favFruit:  []string{"apple", "banana"},
-	}
-	fmt.Println(p)
-	
-
+	account := accounts.NewAccount("youjin")
+	fmt.Println(account)
 }
 
 
