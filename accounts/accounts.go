53edb965e5e554e9eb77461318dd6276354d424e
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-11 22:34:13 +0900

    structs + methods

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -9,3 +9,19 @@ func NewAccount(owner string) *bankAccount {
 	account := bankAccount{owner: owner, balance: 0}
 	return &account
 }
+
+// value receiver
+// receives a copy of the type
+func (a bankAccount) Owner() string {
+	return a.owner
+}
+
+func (a bankAccount) Balance() int {
+	return a.balance
+}
+
+// pointer receiver
+// receives a memory address of the type (an actual value, not a copy)
+func (a *bankAccount) Deposit(amount int) {
+	a.balance += amount
+}
--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 func main() {
 	account := accounts.NewAccount("youjin")
 	fmt.Println(account)
+	fmt.Println(account.Owner())
+	fmt.Println(account.Balance())
+	account.Deposit(10)
+	fmt.Println(account.Balance())
 }
 
 
