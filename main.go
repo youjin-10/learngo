f95f56223f7497186ec7bd9cb609384d6a7c1500
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-04-17 22:22:42 +0900

    job scraper - 1

--- main.go
+++ main.go
@@ -3,54 +3,44 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct{
-	url string
-	status string
-}
-
-var errRequestFailed = errors.New("request failed")
+var baseURL = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	//  create a channel
-	c := make(chan requestResult)
-
-	urls := []string{
-		"https://www.google.com",
-		"https://www.facebook.com",
-		"https://www.youtube.com",
-		"https://www.amazon.com",
-		"https://www.instagram.com",
-		"https://www.linkedin.com",
-		"https://www.reddit.com",
-	}
-
-	// an empty map
-	results := make(map[string]string)
-
-	for _, url := range urls {
-		go hitUrl(url, c)
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c // receiving from channel is blocking operation!!
-
-		results[result.url] = result.status
-	}
-
-	fmt.Println(results)
+	pages := getPages()
+	fmt.Println(pages)
 }
 
-// send-only channel
-func hitUrl(url string, c chan<- requestResult) {
-	resp, err := http.Get(url)
-	status := "OK"
-	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED"
-	} 
-	c <- requestResult{url: url, status: status}
+func getPages() int {
+	resp, err := http.Get(baseURL)
+	checkErr(err)
+	checkStatusCode(resp)
+
+	defer resp.Body.Close() // preventing memory leaks
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each()
+
+	return 0;
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkStatusCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln(errors.New("Request failed with status: " + fmt.Sprint(res.StatusCode)))
+	}
 }
 
 
